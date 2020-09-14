96a2fd32a829ad1e5e8cf51f321f9102b3bcc3f7
Saurav <saurav.satpathy98@gmail.com>
2020-09-14 10:54:04 +0530

    Traditional LookUp-Method(ApplicationContext)

---
+++ IOCProj42-Traditional-LookUpMethod/src/com/nt/beans/Water_Dependent.java
@@ -0,0 +1,13 @@
+package com.nt.beans;
+
+public class Water_Dependent {
+
+	
+	 Water_Dependent(){
+		 System.out.println("Water:O param Constructor");
+	 }
+	public void water() {
+		System.out.println("Lunch is too hot need water");
+	}
+	
+}
